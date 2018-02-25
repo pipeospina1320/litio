390079bd4330bb8b8cb78fce2d6f729c7d54b82e
pipeospina1320 <felipe.1320>
2018-02-25 18:55:01 -0500

    se añade plantilla boostrap se modifica el gitignore

---
+++ src/Controller/InicioController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pipe
+ * Date: 19/02/2018
+ * Time: 1:32 PM
+ */
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class InicioController extends Controller
+{
+
+    /**
+     * @Route("/" , name="inicio")
+     */
+    public function number()
+    {
+        return $this->render('Inicio/index.html.twig');
+    }
+}
