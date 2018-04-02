9a55672a79f9639150de07061b295a70c97e68d2
Pipe Ospina <juanfe-1994@hotmail.com>
2018-04-01 21:40:47 -0500

    se crean los repositorios de las nuevas entidades

---
+++ src/Repository/EntidadPensionRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\EntidadPension;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method EntidadPension|null find($id, $lockMode = null, $lockVersion = null)
+ * @method EntidadPension|null findOneBy(array $criteria, array $orderBy = null)
+ * @method EntidadPension[]    findAll()
+ * @method EntidadPension[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class EntidadPensionRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, EntidadPension::class);
+    }
+
+//    /**
+//     * @return EntidadPension[] Returns an array of EntidadPension objects
+//     */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('e')
+            ->andWhere('e.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('e.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?EntidadPension
+    {
+        return $this->createQueryBuilder('e')
+            ->andWhere('e.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
---
+++ src/Repository/SStipoSaludRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\SStipoSalud;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method SStipoSalud|null find($id, $lockMode = null, $lockVersion = null)
+ * @method SStipoSalud|null findOneBy(array $criteria, array $orderBy = null)
+ * @method SStipoSalud[]    findAll()
+ * @method SStipoSalud[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SStipoSaludRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, SStipoSalud::class);
+    }
+
+//    /**
+//     * @return SStipoSalud[] Returns an array of SStipoSalud objects
+//     */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?SStipoSalud
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
